7123d552c3c8a0dc65a496b135bc33fbdebed18f
thnext <jordanpapy88@gmail.com>
2020-07-07 16:54:14 +0200

    some addition codes (swith case , and Greedy algorithm for general case)

--- CoinChange/Program.cs
+++ CoinChange/Program.cs
@@ -13,25 +13,73 @@ namespace CoinChange
     {
         static void Main(string[] args)
         {
-            // canonical coin systems
-
-            int[] C = new int[] { 100, 50, 20, 10, 5, 2, 1 };
-
-            Console.WriteLine("enter the change:");
-
-            int change = int.Parse(Console.ReadLine());
-
-            if (change == 0)
+            Console.WriteLine("press: 1. for cannonical coin system");
+            Console.WriteLine("press: 2. for your own change system");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
             {
-                Console.WriteLine("not possible");
-            }
-            else if (change < 0)
-            {
-                Console.WriteLine("input should be a positive integer");
-            }
-            else
-            {
-                Makechange(C, change);
+                case 1:
+                    // canonical coin systems
+
+                    int[] C = new int[] { 100, 50, 20, 10, 5, 2, 1 };
+
+                    Console.WriteLine("enter the change:");
+
+                    int change = int.Parse(Console.ReadLine());
+
+                    if (change == 0)
+                    {
+                        Console.WriteLine("not possible");
+                    }
+                    else if (change < 0)
+                    {
+                        Console.WriteLine("wrong input (negative number)");
+                    }
+                    else
+                    {
+                        Makechange(C, change);
+                    }
+                    break;
+
+                case 2:
+                    // Greedy algorithm for general case
+
+                    Console.WriteLine("Enter the numbers of coins you would like to use for the system?");
+                    int numofcoins = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter your coin system in descending order,1 should be the last value.");
+                    int[] coins = new int[numofcoins];
+                    for (int x = 0; x <= numofcoins - 1; x++)
+                    {
+                        coins[x] = int.Parse(Console.ReadLine());
+                    }
+                    if (coins[coins.Length - 1] != 1)
+                    {
+                        Console.WriteLine("You must have 1 at the end!");
+                        _ = Console.ReadKey();// I added this FOR reading the message, the program was ending immediately
+                    }
+                    else if (coins.Any(p => p < 0))
+                    {
+                        Console.WriteLine("wrong input (negative number)");
+                        _ = Console.ReadKey();
+                    }
+                    else if (coins.Length != coins.Distinct().Count())
+                    {
+                        Console.WriteLine("you have Entered same input!");
+                        _ = Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine("How much money do you need back?");
+                        int amount = int.Parse(Console.ReadLine());
+                        Makechange(coins, amount);
+                    }
+
+                    break;
+                default:
+                    Console.WriteLine("invalid option");
+                    _ = Console.ReadKey();
+                    break;
+                    
             }
         }
         public static void Makechange(int[] coins, int change)
